2a0a98f446cba3553cceaa15c968441e1fc4c84b
Xiangru Lian <admin@mail.xrlian.com>
2020-08-21 05:47:45 -0700

    [feat] buffered io

--- bincode-grpc/src/lib.rs
+++ bincode-grpc/src/lib.rs
@@ -8,15 +8,16 @@ pub mod bi_codec {
     use grpcio::Result;
     use serde::de::DeserializeOwned;
     use serde::Serialize;
+    use std::io::{BufReader, BufWriter};
 
     #[inline]
     pub fn ser<M: Serialize, B: BufMut>(msg: &M, buf: &mut B) {
-        bincode::serialize_into(buf.writer(), msg).expect("Writing message to buffer failed");
+        bincode::serialize_into(BufWriter::new(buf.writer()), msg).expect("Writing message to buffer failed");
     }
 
     #[inline]
     pub fn de<M: DeserializeOwned>(reader: MessageReader) -> Result<M> {
-        Ok(bincode::deserialize_from(reader).expect("Reading message from buffer failed"))
+        Ok(bincode::deserialize_from(BufReader::new(reader)).expect("Reading message from buffer failed"))
     }
 }
 
