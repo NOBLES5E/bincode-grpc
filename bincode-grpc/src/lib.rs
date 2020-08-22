7dd3f5bcd8d830cf65ad4aa293ddfcfeecea57b5
Xiangru Lian <admin@mail.xrlian.com>
2020-08-22 03:51:07 -0700

    [perf] tweak deserialize

--- bincode-grpc/src/lib.rs
+++ bincode-grpc/src/lib.rs
@@ -3,20 +3,21 @@ extern crate self as bincode_grpc;
 pub extern crate tracing;
 
 pub mod bi_codec {
-    use bytes::buf::BufMutExt;
     use grpcio::MessageReader;
     use grpcio::Result;
     use serde::de::DeserializeOwned;
     use serde::Serialize;
-    use std::io::{BufReader};
+    use std::io::{BufReader, Read};
 
     pub fn ser<M: Serialize>(msg: &M, buf: &mut Vec<u8>) {
         let serialized = bincode::serialize(msg).expect("serialize message failed");
         assert_eq!(std::mem::replace(buf, serialized).len(), 0);
     }
 
-    pub fn de<M: DeserializeOwned>(reader: MessageReader) -> Result<M> {
-        Ok(bincode::deserialize_from(BufReader::with_capacity(1024 * 512, reader)).expect("Reading message from buffer failed"))
+    pub fn de<M: DeserializeOwned>(mut reader: MessageReader) -> Result<M> {
+        let mut buf = Vec::with_capacity(reader.len());
+        reader.read_to_end(&mut buf).expect("Reading message from buffer failed");
+        Ok(bincode::deserialize(&mut buf).expect("Deserializing message from buffer failed"))
     }
 }
 
